faea8666746bbc559c8d1b8af1f08cc8cd4f1220
Your Name <you@example.com>
2025-03-14 00:25:56 +0900

    dbのえらーがきえた？

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api') // ← ここに'api'を追加（localhost:3001/apiにマッピングされる）
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
