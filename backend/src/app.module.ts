65b6792367fef1591946cf8b6ed4232d37645225
Your Name <you@example.com>
2025-03-14 00:20:16 +0900

    dbのえらーがきえた？

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,21 +1,20 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'database',       // ✅修正済み
-      port: 5432,
-      username: 'user',       // ✅修正済み
-      password: 'password',
-      database: 'mydb',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
       synchronize: true,
     }),
-    
   ],
   controllers: [AppController],
   providers: [AppService],
