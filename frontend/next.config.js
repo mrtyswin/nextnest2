74a9d66e48fbcd16961d2f3259660a45ba033c37
Your Name <you@example.com>
2025-03-14 01:52:57 +0900

    上書きまえ

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: false,
+  swcMinify: true,
+  experimental: {
+    watchDirs: ['./'],  // ファイル変更の監視を明示
+  },
+};
+
+module.exports = nextConfig;
